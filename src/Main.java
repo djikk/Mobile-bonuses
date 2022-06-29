c5e648cabb323debc51e2524481e11d19e51144e
Victor <knightivanhoe1194@gmail.com>
2022-06-29 07:09:40 +0300

    Camelcase

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 
-    public class Main {
-        public static void main(String[] args) {
-            int start_score = 100;
-            int bonus;
-            int add = 1001;
-            if (add <= 1000) {
-                bonus = 0;
-            } else {
-                bonus = add / 100;
-            }
-            int balance = start_score + add+ bonus;
-            System.out.println("Баланс: " + balance);
+public class Main {
+    public static void main(String[] args) {
+        int startScore = 100;
+        int bonus;
+        int add = 1001;
+        if (add <= 1000) {
+            bonus = 0;
+        } else {
+            bonus = add / 100;
         }
+        int balance = startScore + add + bonus;
+        System.out.println("Баланс: " + balance);
+        System.out.println("Бонус: " + bonus);
     }
+}
 
